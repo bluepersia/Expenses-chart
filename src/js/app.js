59044bb5dde91b886d4a1fecdb0c348bf13f7a4d
Marco Zimmermann <fuzzyslash@hotmail.com>
2024-06-13 19:25:16 -0600

    Mobile

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 
 let data;
 
+fetchDataAndRender ();
+
 async function fetchDataAndRender () 
 {
     const res = await fetch ('./data.json');
@@ -10,16 +12,17 @@ async function fetchDataAndRender ()
 
     data = await res.json ();
 
-    renderChart (data);
+    this.renderChart (data);
 }
 
-renderChart (data) 
+function renderChart (data) 
 {
     document.querySelector ('.cmp__chart').innerHTML = data.map (item => `
         <div class='cmp__chart__item'>
             <div class='cmp__chart__inner ${item.day === 'wed' && 'cmp__chart__inner--active'}' style="height:${item.amount * 3}px">
             </div>
             <p class='cmp__chart__day'>${item.day}</p>
+            <h3 class='cmp__chart__label'>$${item.amount}</h3>
         </div>
         ` ).join ('');
 }
